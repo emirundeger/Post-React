9086e6c4a5b367739ed9ab0394634cc9ba4c9e7c
emirundeger <emirundeger@gmail.com>
2020-07-10 15:19:58 +0100

    Working with guards - not showing the route if the user is unauthenticated

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,6 +5,9 @@ import "./Blog.css";
 import { Route, Link, Switch, Redirect } from "react-router-dom";
 
 class Blog extends Component {
+  state = {
+    auth: false
+  };
   render() {
     return (
       <div className="Blog">
@@ -31,7 +34,9 @@ class Blog extends Component {
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
         <Route path="/" render={() => <h1>Home 2</h1>} /> */}
         <Switch>
-          <Route path="/new-post" component={NewPost} />
+          {this.state.auth ? (
+            <Route path="/new-post" component={NewPost} />
+          ) : null}
           <Route path="/posts" component={Posts} />
           <Redirect from="/" to="/posts" />
           {/* <Route path="/" component={Posts} /> */}
