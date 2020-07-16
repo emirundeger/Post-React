8a4d57091ec986e3a45c16e3f25076c23debfaa3
emirundeger <emirundeger@gmail.com>
2020-07-16 11:32:03 +0100

    Redirect added

--- src/containers/Blog/Blog2.js
+++ src/containers/Blog/Blog2.js
@@ -3,7 +3,7 @@ import Courses from "../../components/Courses";
 import Course from "../../components/Course";
 import Users from "../../components/Users";
 import NoMatch from "../../components/NoMatch";
-import { Route, NavLink, Switch } from "react-router-dom";
+import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 
 class Blog2 extends Component {
   render() {
@@ -23,6 +23,7 @@ class Blog2 extends Component {
           <Route path="/users" component={Users} />
           {/* <Route path="/courses/:courseId" component={Course} /> */}
           <Route path="/courses" component={Courses} />
+          <Redirect from="/all-courses" to="/courses" />
           <Route component={NoMatch} />
         </Switch>
       </div>
